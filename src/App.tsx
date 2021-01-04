32531d15243472fffdf5504e5c1033e2f34e743d
Tristan Barrow <tristan.barrow@workmax.com>
2021-01-04 14:45:54 -0700

    map demos setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,52 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import CustomReportsPage from './CustomReportsPage/components/CustomReportsPage';
+import MapBox from './mapbox-maps/MapPage';
+import LeafletMap from './leaflet-maps/MapPage';
+import styled from 'styled-components';
+
+const NavBar = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  height: 34px;
+  background-color: lightgrey;
+`;
+
+const NavLink = styled(Link)`
+  font-weight: 900;
+  text-decoration: none;
+  color: black;
+  transition: all .2s;
+  :hover {
+    cursor: pointer;
+    color: gray;
+  }
+`;
+
 
 function App() {
   return (
-    <CustomReportsPage />
+    <Router>
+      <NavBar>
+        <NavLink to="/mapbox">Mapbox Demo</NavLink>
+        <NavLink to="/leaflet">Leaflet Demo</NavLink>
+        <NavLink to="/custom-reports">Custom Reports</NavLink>
+      </NavBar>
+      
+      <Switch>
+        <Route path='/mapbox'>
+          <MapBox />
+        </Route>
+        <Route path='/leaflet'>
+          <LeafletMap />
+        </Route>
+        <Route path='/custom-reports'>
+          <CustomReportsPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/mapdata/employeeData.js
@@ -0,0 +1,41 @@
+export default {
+    type: "FeatureCollection",
+    crs: { 
+        type: "name", 
+        properties: { 
+            name: "urn:ogc:def:crs:OGC:1.3:CRS84" 
+        } 
+    },
+    features: [
+        {
+            type: "Feature", 
+            geometry: { type: "Point", coordinates: [ -111.634, 39.989] },
+            properties: { id:'1', name: 'Bob Roberto'}
+        },
+        { 
+            type: "Feature", 
+            geometry: { type: "Point", coordinates:[ -111.845, 39.978] },
+            properties: { id:'2', name: 'Dusty Bottoms'}
+        }, 
+        { 
+            type: "Feature", 
+            geometry: { type: "Point", coordinates:[ -111.743, 39.975] },
+            properties: { id:'3', name: 'Carrol Ling'}
+        },
+        { 
+            type: "Feature", 
+            geometry: { type: "Point", coordinates:[ -111.756, 39.976] },
+            properties: { id:'4', name: 'Mike Rofone'}
+        },
+        { 
+            type: "Feature", 
+            geometry: { type: "Point", coordinates:[ -111.791, 39.982] },
+            properties: { id:'5', name: 'Rob Urr'}
+        },
+        { 
+            type: "Feature", 
+            geometry: { type: "Point", coordinates:[ -111.74, 39.978] },
+            properties: { id:'6', name: 'Berry Razz'}
+        }
+    ]
+}
